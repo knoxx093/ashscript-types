e67c9621cfa3e33f457fa9244b8791c348a701f1
Carson <carsonburke22@gmail.com>
2024-10-09 10:37:59 -0700

    hex layout constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,20 @@
 use enum_map::{enum_map, EnumMap};
+use glam::Vec2;
 use hashbrown::HashSet;
+use hexx::{HexLayout, HexOrientation};
 use lazy_static::lazy_static;
 
 use crate::{resource::Resource, unit::UnitPart};
+
+pub const HEX_SIZE: Vec2 = Vec2::splat(64.0);
+pub const CHUNK_SIZE: u32 = 5;
+pub const HEX_LAYOUT: HexLayout = HexLayout {
+    hex_size: HEX_SIZE,
+    orientation: HexOrientation::Pointy,
+    origin: Vec2::new(0., 0.),
+    invert_x: false,
+    invert_y: false,
+};
 
 pub const UNIT_AGE_EXP: f32 = 0.4;
 pub const AGE_PER_GEN_PART: u32 = 10;
