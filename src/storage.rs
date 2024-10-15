af45d1830baf59ca53d16823af41fa12309e7759
Carson <carsonburke22@gmail.com>
2024-10-14 22:26:07 -0700

    add Storage struct

---
+++ src/storage.rs
@@ -0,0 +1,20 @@
+use hashbrown::{HashMap, HashSet};
+use serde::Serialize;
+
+use crate::{resource::Resource};
+
+#[derive(Default, Serialize, Clone)]
+pub struct Storage {
+    pub resources: HashMap<Resource, u32>,
+    /// resources that are allowed to be inserted into the structure. If none, accept all resources
+    pub allowed_inputs: Option<HashSet<Resource>>,
+    pub capacity: u32,
+}   
+
+impl Storage {
+    pub fn new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+}
