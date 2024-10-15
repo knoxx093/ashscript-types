3f3b7ce3431f581fd28d0acea1989a9d641651c8
Carson <carsonburke22@gmail.com>
2024-10-14 21:23:00 -0700

    game object getter and Identifier

--- src/map.rs
+++ src/map.rs
@@ -1,7 +1,7 @@
 use hexx::Hex;
 use serde::Serialize;
 
-use crate::{chunk::{Chunk, Chunks}, constants::map::CHUNK_SIZE, unit::Unit};
+use crate::{chunk::{Chunk, Chunks}, constants::map::CHUNK_SIZE, objects::{GameObject, GameObjectKind, GameObjectMut, HasHealth, Identifier}, structures::{factory::Factory, turret::Turret}, unit::Unit};
 
 #[derive(Default, Serialize, Clone)]
 pub struct Map {
@@ -20,9 +20,75 @@ impl Map {
         self.chunks.get(&chunk_hex)
     }
 
+    pub fn chunk_at_mut(&mut self, hex: &Hex) -> Option<&mut Chunk> {
+        let chunk_hex = hex.to_lower_res(CHUNK_SIZE);
+        self.chunks.get_mut(&chunk_hex)
+    }
+
     pub fn unit_at(&self, hex: &Hex) -> Option<&Unit> {
         let chunk = self.chunk_at(hex)?;
-
         chunk.units.get(hex)
+    }
+
+    pub fn unit_at_mut(&mut self, hex: &Hex) -> Option<&mut Unit> {
+        let chunk = self.chunk_at_mut(hex)?;
+        chunk.units.get_mut(hex)
+    }
+
+    pub fn factory_at(&self, hex: &Hex) -> Option<&Factory> {
+        let chunk = self.chunk_at(hex)?;
+        chunk.factories.get(hex)
+    }
+
+    pub fn factory_at_mut(&mut self, hex: &Hex) -> Option<&mut Factory> {
+        let chunk = self.chunk_at_mut(hex)?;
+        chunk.factories.get_mut(hex)
+    }
+
+    pub fn turret_at(&self, hex: &Hex) -> Option<&Turret> {
+        let chunk = self.chunk_at(hex)?;
+        chunk.turrets.get(hex)
+    }
+
+    pub fn turret_at_mut(&mut self, hex: &Hex) -> Option<&mut Turret> {
+        let chunk = self.chunk_at_mut(hex)?;
+        chunk.turrets.get_mut(hex)
+    }
+
+    pub fn game_object(&self, identifier: &Identifier) -> Option<GameObject> {
+
+        match identifier.kind {
+            GameObjectKind::Unit => {
+                let object = self.unit_at(&identifier.hex)?;
+                Some(GameObject::Unit(object))
+            },
+            GameObjectKind::Turret => {
+                let object = self.turret_at(&identifier.hex)?;
+                Some(GameObject::Turret(object))
+            }
+            GameObjectKind::Factory => {
+                let object = self.factory_at(&identifier.hex)?;
+                Some(GameObject::Factory(object))
+            }
+            _ => None,
+        }
+    }
+
+    pub fn game_object_mut<T>(&mut self, identifier: &Identifier) -> Option<GameObjectMut> {
+        match identifier.kind {
+            GameObjectKind::Unit => {
+                let object = self.unit_at_mut(&identifier.hex)?;
+                Some(GameObjectMut::Unit(object))
+            }
+            GameObjectKind::Turret => {
+                let object = self.turret_at_mut(&identifier.hex)?;
+                Some(GameObjectMut::Turret(object))
+            }
+            GameObjectKind::Factory => {
+                let object = self.factory_at_mut(&identifier.hex)?;
+                Some(GameObjectMut::Factory(object))
+            }
+            _ => None,
+        }
     }
 }
--- src/objects.rs
+++ src/objects.rs
@@ -1,4 +1,7 @@
+use hexx::Hex;
 use uuid::Uuid;
+
+use crate::{structures::{factory::Factory, turret::Turret}, unit::Unit};
 
 
 
@@ -14,4 +17,28 @@ pub enum Attackable {
 
 pub trait HasId {
     fn id(&self) -> Uuid;
+}
+
+pub enum GameObject<'a> {
+    Unit(&'a Unit),
+    Turret(&'a Turret),
+    Factory(&'a Factory),
+}
+
+pub enum GameObjectMut<'a> {
+    Unit(&'a mut Unit),
+    Turret(&'a mut Turret),
+    Factory(&'a mut Factory),
+}
+
+/// Each type has its own storage inside of a chunk, and cannot share a hex/tile with another of its own type (ex: two units cannot be on the same tile)
+pub enum GameObjectKind {
+    Unit,
+    Turret,
+    Factory,
+}
+
+pub struct Identifier {
+    pub hex: Hex,
+    pub kind: GameObjectKind,
 }
