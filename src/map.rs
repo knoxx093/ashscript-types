a1eb3b66ae5ee128949dccecac46e62836727890
Carson <carsonburke22@gmail.com>
2024-11-06 21:47:57 -0800

    entity at util for Map

--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,7 @@
 use std::any::TypeId;
 
 use hashbrown::HashMap;
+use hecs::Entity;
 use hexx::Hex;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -35,6 +36,11 @@ impl Map {
     pub fn chunk_at_mut(&mut self, hex: &Hex) -> Option<&mut Chunk> {
         let chunk_hex = hex.to_lower_res(CHUNK_SIZE);
         self.chunks.get_mut(&chunk_hex)
+    }
+
+    pub fn entity_at(&self, hex: &Hex, kind: GameObjectKind) -> Option<&Entity> {
+        let chunk = self.chunk_at(hex)?;
+        chunk.entities[kind].get(hex)
     }
 }
 
