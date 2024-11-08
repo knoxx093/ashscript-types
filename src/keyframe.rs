992f6f5c41a1702e40df7f67afd9e772bc6f4087
Carson <carsonburke22@gmail.com>
2024-11-07 20:10:36 -0800

    circumvent force cloning of World

--- src/keyframe.rs
+++ src/keyframe.rs
@@ -15,7 +15,7 @@ pub struct KeyFrame {
 }
 
 impl KeyFrame {
-    pub fn from_existing(map: Map, world: World, global: Global, actions: ActionsByKind) -> Self {
+    pub fn from_existing(map: Map, world: &World, global: Global, actions: ActionsByKind) -> Self {
         Self {
             map,
             world_data: serialize_world_data(&world),
