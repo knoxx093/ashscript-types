de1352e5f38bea75172e925496b0020b613255e5
Carson <carsonburke22@gmail.com>
2024-11-07 21:49:30 -0800

    terrain kind property

--- src/components/terrain.rs
+++ src/components/terrain.rs
@@ -1,7 +1,19 @@
+use std::default;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Serialize, Deserialize, Clone, Debug)]
-pub struct Terrain;
+pub struct Terrain {
+    pub kind: TerrainKind,
+}
+
+#[derive(Default, Serialize, Deserialize, Clone, Debug)]
+pub enum TerrainKind {
+    Wall,
+    Lava,
+    #[default]
+    Plain,
+}
 
 #[derive(Default, Serialize, Deserialize, Clone, Debug)]
 pub struct Wall;
