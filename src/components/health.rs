a3166428ed1e2528320cd00d678ff0ff3e9830a4
Carson <carsonburke22@gmail.com>
2024-11-10 20:48:50 -0800

    for kind and unit health utils

--- src/components/health.rs
+++ src/components/health.rs
@@ -1,5 +1,9 @@
 use serde::{Deserialize, Serialize};
 
+use crate::objects::GameObjectKind;
+
+use super::body::UnitBody;
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Health(pub u32);
 
@@ -7,4 +11,26 @@ impl Default for Health {
     fn default() -> Self {
         Self(100)
     }
+}
+
+impl Health {
+    pub fn from_kind(kind: GameObjectKind) -> Self {
+        match kind {
+            GameObjectKind::Unit => Self(10),
+            GameObjectKind::Turret => Self(100),
+            _ => Self::default(),
+        }
+    }
+
+    pub fn for_unit(body: &UnitBody) -> Self {
+        Self(body.health_without_shields())
+    }
+
+    pub fn current_with_shields() {
+        todo!()
+    }
+
+    pub fn max_with_shields() {
+        todo!()
+    }   
 }
