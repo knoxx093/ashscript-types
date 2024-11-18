1fa2b2b569d768ce5d460406190fd64abe6ef9fc
Carson <carsonburke22@gmail.com>
2024-11-18 14:12:21 -0800

    turbine and solar panel output_chunk util

--- src/components/solar_panel.rs
+++ src/components/solar_panel.rs
@@ -1,7 +1,7 @@
 use hexx::Hex;
 use serde::{Deserialize, Serialize};
 
-use crate::{constants::structures::{SOLAR_OUTPUT, WIND_OUTPUT}, map::Map};
+use crate::{chunk::Chunk, constants::structures::{SOLAR_OUTPUT, WIND_OUTPUT}, map::Map};
 
 #[derive(Serialize, Default, Deserialize, Clone, Debug)]
 pub struct SolarPanel {}
@@ -10,6 +10,10 @@ impl SolarPanel {
     pub fn output(&self, map: &Map, hex: &Hex) -> Option<u32> {
         let chunk = map.chunk_at(hex)?;
 
-        Some((chunk.solar_efficiency * (SOLAR_OUTPUT as f32)) as u32)
+        Some(self.output_chunk(chunk))
     }   
+
+    pub fn output_chunk(&self, chunk: &Chunk) -> u32 {
+        (chunk.solar_efficiency * (SOLAR_OUTPUT as f32)) as u32
+    }
 }
--- src/components/turbine.rs
+++ src/components/turbine.rs
@@ -1,7 +1,7 @@
 use hexx::Hex;
 use serde::{Deserialize, Serialize};
 
-use crate::{constants::structures::WIND_OUTPUT, map::Map};
+use crate::{chunk::Chunk, constants::structures::WIND_OUTPUT, map::Map};
 
 #[derive(Serialize, Default, Deserialize, Clone, Debug)]
 pub struct Turbine {
@@ -12,6 +12,10 @@ impl Turbine {
     pub fn output(&self, map: &Map, hex: &Hex) -> Option<u32> {
         let chunk = map.chunk_at(hex)?;
 
-        Some((chunk.wind_efficiency * (WIND_OUTPUT as f32)) as u32)
+        Some(self.output_chunk(chunk))
     }   
+
+    pub fn output_chunk(&self, chunk: &Chunk) -> u32 {
+        (chunk.wind_efficiency * (WIND_OUTPUT as f32)) as u32
+    }
 }
