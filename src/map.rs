fd3d55a9f9dc477c90a27ca6d48aea2f805ef233
Carson <carsonburke22@gmail.com>
2024-11-18 13:53:32 -0800

    chunk searching utility on Map

--- src/map.rs
+++ src/map.rs
@@ -1,8 +1,8 @@
 use std::any::TypeId;
 
-use hashbrown::HashMap;
+use hashbrown::{HashMap, HashSet};
 use hecs::Entity;
-use hexx::Hex;
+use hexx::{shapes::hexagon, Hex};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
@@ -44,6 +44,26 @@ impl Map {
     pub fn remove_entity(&mut self, hex: &Hex, kind: GameObjectKind) -> Option<Entity> {
         let chunk = self.chunk_at_mut(hex)?;
         chunk.entities[kind].remove(hex)
+    }
+
+    pub fn chunks_in_area(&mut self, hex: Hex, radius: u32) -> Vec<&Chunk> {
+        let mut found_chunk_hexes = HashSet::new();
+        let mut chunks = Vec::new();
+
+        for search_hex in hexagon(hex, radius) {
+            let chunk_hex = hex.to_lower_res(CHUNK_SIZE);
+            if found_chunk_hexes.contains(&chunk_hex) {
+                continue;
+            }
+
+            found_chunk_hexes.insert(chunk_hex);
+
+            if let Some(chunk) = self.chunk_at(&search_hex) {
+                chunks.push(chunk);
+            }
+        }
+
+        chunks
     }
 }
 
